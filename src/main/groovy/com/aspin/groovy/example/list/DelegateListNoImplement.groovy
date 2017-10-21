dcf33fdcbf9c7cdfedc524f27d55df801e6170e4
Kevin Chen <kevin.chen@fitbit.com>
2017-10-21 18:18:45 -0400

    add a no implement version

--- src/main/groovy/com/aspin/groovy/example/list/DelegateListNoImplement.groovy
+++ src/main/groovy/com/aspin/groovy/example/list/DelegateListNoImplement.groovy
@@ -1,123 +1,100 @@
 package com.aspin.groovy.example.list
 
-class DelegateList {
+class DelegateListNoImplement {
     private List<String> lowerCaseStrings = []
     private DelegatingListImplementation uppercaseStrings = new DelegatingListImplementation()
 
-    private abstract static class DelegatingList implements List<String> {
+    private abstract static class DelegatingList {
         abstract List<String> getDelegate()
 
-        @Override
         int size() {
             return delegate.size()
         }
 
-        @Override
         boolean isEmpty() {
             return delegate.isEmpty()
         }
 
-        @Override
         boolean contains(Object o) {
             return delegate.contain(o)
         }
 
-        @Override
         Iterator<String> iterator() {
             return delegate.iterator()
         }
 
-        @Override
         Object[] toArray() {
             return delegate.toArray()
         }
 
-        @Override
         def <T> T[] toArray(T[] a) {
             return delegate.toArray(a)
         }
 
-        @Override
         boolean add(String s) {
             return delegate.add(s)
         }
 
-        @Override
         boolean remove(Object o) {
             return delegate.remove(o)
         }
 
-        @Override
         boolean containsAll(Collection<?> c) {
             return delegate.containsAll(c)
         }
 
-        @Override
         boolean addAll(Collection<? extends String> c) {
             return delegate.addAll(c)
         }
 
-        @Override
         boolean addAll(int index, Collection<? extends String> c) {
             return delegate.addAll(index, c)
         }
 
-        @Override
         boolean removeAll(Collection<?> c) {
             return delegate.removeAll(c)
         }
 
-        @Override
         boolean retainAll(Collection<?> c) {
             return delegate.retainAll(c)
         }
 
-        @Override
         void clear() {
             delegate.clear()
         }
 
-        @Override
         String get(int index) {
             return delegate.get(index)
         }
 
-        @Override
         String set(int index, String element) {
             return delegate.set(index, element)
         }
 
-        @Override
         void add(int index, String element) {
             delegate.add(index, element)
         }
 
-        @Override
         String remove(int index) {
             return delegate.remove(index)
         }
 
-        @Override
         int indexOf(Object o) {
             return delegate.indexOf(o)
         }
 
-        @Override
         int lastIndexOf(Object o) {
             return delegate.lastIndexOf(o)
         }
 
-        @Override
         ListIterator<String> listIterator() {
             return delegate.listIterator()
         }
 
-        @Override
         ListIterator<String> listIterator(int index) {
             return delegate.listIterator(index)
         }
 
-        @Override
         List<String> subList(int fromIndex, int toIndex) {
             return delegate.subList(fromIndex, toIndex)
         }
@@ -130,16 +107,16 @@ class DelegateList {
         }
     }
 
-    DelegateList(Collection<String> strings) {
+    DelegateListNoImplement(Collection<String> strings) {
         lowerCaseStrings = strings.toList().collect { it.toLowerCase() }
     }
 
-    List<String> getUppercase() {
+    DelegatingList getUppercase() {
         return uppercaseStrings
     }
 
     static void main(String[] args) {
-        def d = new DelegateList(['hello', 'bye'])
+        def d = new DelegateListNoImplement(['hello', 'bye'])
         println d.getUppercase().size() // StackOverflowError
     }
 }
